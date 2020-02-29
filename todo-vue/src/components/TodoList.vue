cfef015530bc4be3cf1a60f393b96107c0464196
Leutrim <leutrimnuredini7@gmail.com>
2020-03-01 00:53:09 +0100

    component

--- todo-vue/src/components/TodoList.vue
+++ todo-vue/src/components/TodoList.vue
@@ -7,14 +7,27 @@
       v-model="newTodo"
       @keyup.enter="addTodo"
     />
-  <transition-group name="fade" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
-    <todo-item v-for="(todo, index) in todosFiltered" :key="todo.id" :todo="todo" :index="index">
-      
-    </todo-item>
-  </transition-group>
+    <transition-group
+      name="fade"
+      enter-active-class="animated fadeInUp"
+      leave-active-class="animated fadeOutDown"
+    >
+      <todo-item
+        v-for="todo in todosFiltered"
+        :key="todo.id"
+        :todo="todo"
+        :checkAll="!anyRemaining"
+        @removedTodo="removeTodo"
+        @finishedEdit="finishedEdit"
+      ></todo-item>
+    </transition-group>
 
     <div class="extra-container">
-      <div><label><input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos">Check All</label></div>
+      <div>
+        <label>
+          <input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos" />Check All
+        </label>
+      </div>
       <div>{{ remaining }} items left</div>
     </div>
 
@@ -27,7 +40,7 @@
 
       <div>
         <transition name="fade">
-         <button v-if="showClearCompleteButton" @click="clearCompleted">Clear Completed</button>
+          <button v-if="showClearCompleteButton" @click="clearCompleted">Clear Completed</button>
         </transition>
       </div>
     </div>
@@ -35,18 +48,18 @@
 </template>
 
 <script>
-import TodoItem from './TodoItem'
+import TodoItem from "../components/TodoItem";
 export default {
   name: "todo-list",
   components: {
-     TodoItem,
+    TodoItem
   },
   data() {
     return {
       newTodo: "",
       idForTodo: 3,
-      beforeEditCache: '',
-      filter: 'all',
+      beforeEditCache: "",
+      filter: "all",
       todos: [
         {
           id: 1,
@@ -65,25 +78,25 @@ export default {
   },
 
   computed: {
-      remaining(){
-        return this.todos.filter(todo => !todo.completed).length
-      },
-      anyRemaining() {
-        return this.remaining != 0
-      },
-      todosFiltered(){
-        if(this.filter == 'all') {
-          return this.todos
-        } else if(this.filter == 'active') {
-          return this.todos.filter(todo => !todo.completed)
-        } else if(this.filter == 'completed') {
-          return this.todos.filter(todo => todo.completed)
-        }
-         return this.todos
-      },
-      showClearCompleteButton(){
-        return this.todos.filter(todo => todo.completed).length > 0
+    remaining() {
+      return this.todos.filter(todo => !todo.completed).length;
+    },
+    anyRemaining() {
+      return this.remaining != 0;
+    },
+    todosFiltered() {
+      if (this.filter == "all") {
+        return this.todos;
+      } else if (this.filter == "active") {
+        return this.todos.filter(todo => !todo.completed);
+      } else if (this.filter == "completed") {
+        return this.todos.filter(todo => todo.completed);
       }
+      return this.todos;
+    },
+    showClearCompleteButton() {
+      return this.todos.filter(todo => todo.completed).length > 0;
+    }
   },
 
   methods: {
@@ -101,34 +114,39 @@ export default {
       this.idForTodo++;
     },
     editTodo(todo) {
-      this.beforeEditCache = todo.title
+      this.beforeEditCache = todo.title;
       todo.editing = true;
     },
     doneEdit(todo) {
-      if (todo.title.trim().length == 0) {
+      if (todo.title.trim() == "") {
         todo.title = this.beforeEditCache;
       }
       todo.editing = false;
     },
     cancelEdit(todo) {
-      todo.title = this.beforeEditCache
-      todo.editing = false
+      todo.title = this.beforeEditCache;
+      todo.editing = false;
     },
-    removeTodo(index) {
+    removeTodo(id) {
+      const index = this.todos.findIndex(item => item.id == id);
       this.todos.splice(index, 1);
     },
-    checkAllTodos(){
-      this.todos.forEach((todo) => todo.completed = event.target.checked)
+    checkAllTodos() {
+      this.todos.forEach(todo => (todo.completed = event.target.checked));
     },
-    clearCompleted(){
-      this.todos = this.todos.filter(todo => !todo.completed)
+    clearCompleted() {
+      this.todos = this.todos.filter(todo => !todo.completed);
+    },
+    finishedEdit(data) {
+      const index = this.todos.findIndex(item => item.id == data.id);
+      this.todos.splice(index, 1, data);
     }
   }
 };
 </script>
 
 <style>
-  @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
+@import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
 
 .todo-input {
   width: 100%;
@@ -146,14 +164,12 @@ export default {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  animation-duration: 0.8s
+  animation-duration: 0.8s;
 }
 
 .remove-item {
   cursor: pointer;
-  position: relative;
-  top: -45px;
-  margin-left: 534px;
+  margin-left: 14px;
   &:hover {
     color: black;
   }
@@ -181,39 +197,41 @@ export default {
   }
 }
 
- .completed {
-    text-decoration: line-through;
-    color: grey;
-  }
+.completed {
+  text-decoration: line-through;
+  color: grey;
+}
 
-  .extra-container {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    font-size: 16px;
-    border-top: 1px solid lightgrey;
-    padding-top: 14px;
-    margin-bottom: 14px;
-  }
-  button {
-    font-size: 14px;
-    background-color: white;
-    appearance: none;
-    &:hover {
-      background: lightgreen;
-    }
-    &:focus {
-      outline: none;
-    }
-  }
-  .active {
+.extra-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  border-top: 1px solid lightgrey;
+  padding-top: 14px;
+  margin-bottom: 14px;
+}
+button {
+  font-size: 14px;
+  background-color: white;
+  appearance: none;
+  &:hover {
     background: lightgreen;
   }
+  &:focus {
+    outline: none;
+  }
+}
+.active {
+  background: lightgreen;
+}
 
-  .fade-enter-active, .fade-leave-active {
-    transition: opacity .2s;
-  }
-  .fade-enter, .fade-leave-to {
-    opacity: 0;
-  }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
